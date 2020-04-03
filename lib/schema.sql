01ef94f80fab06303d7cd14d4cbaf18ab11f5eb0
taroj khan <tarujkhan@yahoo.com>
2020-04-03 21:29:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,4 +8,4 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year DATE, series_id INT
 
 CREATE TABLE characters(id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
 
-CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER)''
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER);
