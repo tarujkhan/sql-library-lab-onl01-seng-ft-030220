7b43d273a1253873d4f0caec3b469483e6bf6dd0
taroj khan <tarujkhan@yahoo.com>
2020-04-03 23:21:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,6 @@ end
 
 def select_name_and_series_subgenres_of_authors
    "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
-end
   
 end
 
