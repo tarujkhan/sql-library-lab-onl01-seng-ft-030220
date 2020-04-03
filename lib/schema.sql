2e2cd159d7ab1abaf3dab767bb58ed184271b351
taroj khan <tarujkhan@yahoo.com>
2020-04-03 21:26:17 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,4 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
-
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
 
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
