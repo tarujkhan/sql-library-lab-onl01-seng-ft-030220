964ebe75bc2b7de4cf51c250f8e9b39d2314b8ef
taroj khan <tarujkhan@yahoo.com>
2020-04-03 21:25:53 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -5,4 +5,7 @@ INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
 
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER)
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
+
